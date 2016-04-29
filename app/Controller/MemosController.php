9c9a0bc18a382fb66b5c687692c2b30e976fa149
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-04-29 13:33:08 +0900

    D-22
    s-1#1
    ---
    1. controller
    
        ==> done

---
+++ app/Controller/MemosController.php
@@ -0,0 +1,19 @@
+<?php
+
+class MemosController extends AppController {
+	
+// 	public $scaffold;
+	
+	public $helpers = array('Html', 'Form');
+// 	public $helpers = array('Html', 'Form');
+
+	//REF http://book.cakephp.org/2.0/en/core-libraries/components/pagination.html
+	public $components = array('Paginator');
+
+	public function index() {
+		
+		$this->render("/Elements/commons/common_1");
+		
+	}//index
+	
+}
