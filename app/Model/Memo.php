996c1285f60391076f3718926e135e3cc66dbf3c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2016-04-29 14:02:48 +0900

    D-22
    s-1#1.steps.~4.2
    ---
    1. new table: local
    
        ==> done

---
+++ app/Model/Memo.php
@@ -0,0 +1,20 @@
+<?php
+
+class Memo extends AppModel {
+	
+	var $name = 'Memo';
+	
+// 	var $belongsTo = 'Genre';
+	
+// 	var $hasMany = array(
+				
+// 			'Position' => array(
+						
+// 					'className' => 'Position'
+// 			)
+				
+// 	);
+	
+	
+	
+}
