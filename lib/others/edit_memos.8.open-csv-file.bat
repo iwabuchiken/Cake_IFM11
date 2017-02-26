8f485e998987ac044470a8206c5b2e67467166b1
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-02-26 13:17:17 +0900

    D-11
    s-286
    ---
    1. ==> memo added

--- lib/others/edit_memos.8.open-csv-file.bat
+++ lib/others/edit_memos.8.open-csv-file.bat
@@ -1,4 +1,7 @@
 
 "C:\Program Files (x86)\sakura\sakura.exe" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
+REM start "C:\Program Files (x86)\sakura\sakura.exe" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
 
-pause
+exit
+
+REM pause
