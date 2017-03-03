157287bf39dfd0192c2dd0e684723e1949c4e1a7
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-03-03 16:43:08 +0900

    D-11
    s-288
    ---
    1. ==> memo added
    2. code cleanup.

--- lib/others/edit_memos.1.bat
+++ lib/others/edit_memos.1.bat
@@ -3,4 +3,6 @@ pushd C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others
 REM use_sqlite_2.rb f
 use_sqlite_2.rb f | clip
 
+C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\edit_memos.2.start-openoffice.bat
+
 pause
--- lib/others/edit_memos.2.start-openoffice.bat
+++ lib/others/edit_memos.2.start-openoffice.bat
@@ -3,4 +3,4 @@ REM start "C:\Program Files (x86)\OpenOffice 4\program\soffice.exe"
 REM "C:\Program Files (x86)\OpenOffice 4\program\soffice.exe" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
 start "C:\Program Files (x86)\OpenOffice 4\program\soffice.exe" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
 
-pause
+REM pause
