631c256def70a4df5a0d738b3434b1e36dd37321
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-03-14 13:43:11 +0900

    D-11
    s-292
    ---
    1. ==> memo added

--- lib/others/edit_memos.5.copy-image-files.bat
+++ lib/others/edit_memos.5.copy-image-files.bat
@@ -1,3 +1,4 @@
-copy C:\Users\iwabuchiken\data\images\iphone\* C:\Users\iwabuchiken\data\images\iphone_to_upload\
+rem copy C:\Users\iwabuchiken\data\images\iphone\* C:\Users\iwabuchiken\data\images\iphone_to_upload\
+move C:\Users\iwabuchiken\data\images\iphone\* C:\Users\iwabuchiken\data\images\iphone_to_upload\
 
 pause
