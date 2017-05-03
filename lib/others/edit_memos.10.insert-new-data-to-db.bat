e92a28f45d811d476bf53d786c230662b0149f49
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-05-03 11:22:25 +0900

    D-11
    s-315
    ---
    1. ==> memo added

---
+++ lib/others/edit_memos.10.insert-new-data-to-db.bat
@@ -0,0 +1,4 @@
+pushd "C:\Program Files (x86)\Google\Chrome\Application"
+start chrome.exe "http://localhost/Eclipse_Luna/Cake_IFM11/images/manage_IPhone_ImageFiles?db_file=ifm11_backup_20160110_080900.bk"
+
+pause
