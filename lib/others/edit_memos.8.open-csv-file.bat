1089a44fa9ef3c6eb5ce58e30c65dbe6a402c0c5
iwabuchiken <iwabuchi.k.2010@gmail.com>
2017-09-11 13:50:28 +0900

    Prog / D-34 3#1 / Cake_IFM11 fix-2 / adjust to WS_Cake_IFM11 / 20170911_124131
    steps=~2.2
    ---
    #) insert ===> data
    
    	==> done

--- lib/others/edit_memos.8.open-csv-file.bat
+++ lib/others/edit_memos.8.open-csv-file.bat
@@ -1,6 +1,12 @@
 
-"C:\Program Files (x86)\sakura\sakura.exe" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
+REM "C:\Program Files (x86)\sakura\sakura.exe" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
+REM start "C:\WORKS_2\Programs\sakura\sakura" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
+"C:\WORKS_2\Programs\sakura\sakura" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
+REM "C:\WORKS_2\Programs\sakura\sakura" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
+
 REM start "C:\Program Files (x86)\sakura\sakura.exe" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
+
+REM pause
 
 exit
 
