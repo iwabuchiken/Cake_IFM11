d3b13f872b3f2a782f875404a3f241672cd54283
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-09-19 11:38:34 +0900

    periodical addtitions

--- lib/others/edit_memos.1.bat
+++ lib/others/edit_memos.1.bat
@@ -3,6 +3,7 @@ pushd C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others
 REM use_sqlite_2.rb f
 use_sqlite_2.rb f | clip
 
-C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\edit_memos.2.start-openoffice.bat
+REM comment out : 20180919_091748
+REM C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\edit_memos.2.start-openoffice.bat
 
 REM pause
