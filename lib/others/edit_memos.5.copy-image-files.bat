f241705fdd58ef975294f2ddfab739350b5985ec
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-11-02 23:56:25 +0900

    periodical addtitions

--- lib/others/edit_memos.5.copy-image-files.bat
+++ lib/others/edit_memos.5.copy-image-files.bat
@@ -2,4 +2,10 @@ rem copy C:\Users\iwabuchiken\data\images\iphone\* C:\Users\iwabuchiken\data\ima
 REM move C:\Users\iwabuchiken\data\images\iphone\* C:\Users\iwabuchiken\data\images\iphone_to_upload\
 move C:\WORKS_2\WS\WS_Cake_IFM11\iphone\* C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
 
+
+REM 2019/10/28 09:12:23
+start C:\WORKS_2\WS\WS_Cake_IFM11\commands\16)_move-mov-files.bat
+start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\11_guitar\memos_guitar.odt
+
+
 REM pause
