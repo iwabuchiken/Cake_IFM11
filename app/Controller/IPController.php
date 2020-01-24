616b8de80f39254782b4bed34d298729738c002d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-01-24 17:30:44 +0900

    res free# JVEMV6 37#12.5_2 / 37. miscs / 12. programming / 5. image-processing /  20200124_172131
    ---------------
    1. T-1.1
    1. DUP : controller : FxTestController.php
    
                            ==> comp

---
+++ app/Controller/IPController.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\
+	=> app diretory
+/cake_apps/Cake_IFM11
+
+ * 
+ * 
+ */
+
+
+class IPController extends AppController {
+
+	
+// 	public $scaffold;
+
+	//ref http://stackoverflow.com/questions/4252577/cakephp-session-cant-write answered Nov 23 '10 at 3:56
+	public $helpers = array('Html', 'Form');
+
+	//REF http://book.cakephp.org/2.0/en/core-libraries/components/pagination.html
+// 	public $components = array('Paginator', 'Session');
+	public $components = array('Paginator');
+
+	public function index() {
+
+		/******************
+		 * step : 1
+		 * 		prep
+		 ****************/
+		
+		/******************
+		 * step : 1.2
+		 * 		prep : url params
+		 ****************/
+		/******************
+		 * step : 1.1 : 1
+		 * 		get : param
+		 ****************/
+		
+	}//public function index()
+
+}//class FxTesterController extends AppController {
+
