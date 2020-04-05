6505b9a3f66b04acb4b6f39b98fc6cfb9d8387e5
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-04-05 14:07:07 +0900

    res free# JVEMV6 44#10.2_ 56_2 / 44. currency / 10. prog-php / 2. tester /  56. prog:php:utils / 20200405_125159

--- app/Controller/FxUtilitiesController.php
+++ app/Controller/FxUtilitiesController.php
@@ -75,9 +75,9 @@ class FxUtilitiesController extends AppController {
 		//ref https://www.php.net/manual/en/function.file-get-contents.php
 		$linesOf_Statememt_File = file_get_contents($fpath_Statement_File);
 		
-		debug("count(\$linesOf_Statememt_File) : " . count($linesOf_Statememt_File));
+// 		debug("count(\$linesOf_Statememt_File) : " . count($linesOf_Statememt_File));
 		//ref https://www.php.net/manual/en/function.substr.php
-		debug("substr(\$linesOf_Statememt_File, 0, 10) : '" . substr($linesOf_Statememt_File, 0, 10) . "'");
+// 		debug("substr(\$linesOf_Statememt_File, 0, 10) : '" . substr($linesOf_Statememt_File, 0, 10) . "'");
 		
 // 		$fin__Statement_File = fopen($fpath_Statement_File, $modeOf_Open_File);
 		
@@ -124,24 +124,51 @@ class FxUtilitiesController extends AppController {
 			$tr = $TRs->item($i);
 // 			$tr = $TRs->childNodes[0];
 			
-			debug("(\$i = $i) \$tr->nodeValue : " . $tr->nodeValue);
+// 			debug("(\$i = $i) \$tr->nodeValue : " . $tr->nodeValue);
 			
 			//test
 			$tr_Child_Nodes = $tr->childNodes;
 			
+			$TDs = $tr->getElementsByTagName('td');
+			
 			// numeric ?
 			$is_Nove_Value_Numeric = is_numeric($tr_Child_Nodes->item(0)->nodeValue);
 			
-			debug("\$tr_Child_Nodes->item(0)->nodeValue : " . $tr_Child_Nodes->item(0)->nodeValue
+// 			debug("\$tr_Child_Nodes->item(0)->nodeValue : " . $tr_Child_Nodes->item(0)->nodeValue
 					
-					. "(" . "numerical => " 
-					. ( $is_Nove_Value_Numeric? "true" : "false" . ")")
-					);
+// 					. "(" . "numerical => " 
+// 					. ( $is_Nove_Value_Numeric? "true" : "false" . ")")
+// 					);
 			
 			// push
-			if ($is_Nove_Value_Numeric == true) {
+			$tds_Item_2 = $TDs->item(2);
+			
+			$keyWord_Omit = "balance";
+			
+			debug("\$tr->nodeValue : " . $tr->nodeValue . " / \$tds_Item_2 : " . $tds_Item_2->nodeValue);
+			
+			debug(($tds_Item_2->nodeValue != $keyWord_Omit) ? "omit : TRUE" : "omit : FALSE");
+			debug("\$tds_Item_2->nodeValue == \$keyWord_Omit --> " . (($tds_Item_2->nodeValue == $keyWord_Omit) ? "omit : TRUE" : "omit : FALSE"));
+// 			debug(($tds_Item_2 !== $keyWord_Omit) ? "omit : TRUE" : "omit : FALSE");
+// 			debug("strcmp(\$tds_Item_2, \$keyWord_Omit)" . strcmp($tds_Item_2, $keyWord_Omit));
+// // 			debug(($tds_Item_2 != $keyWord_Omit) ? "omit : TRUE" : "omit : FALSE");
+			
+// 			debug("\$is_Nove_Value_Numeric : " . ($is_Nove_Value_Numeric == true ? "TRUE" : "FALSE"));
+			
+// 			if ($is_Nove_Value_Numeric == true) {
+// 			if ($is_Nove_Value_Numeric == true && $tds_Item_2 != $keyWord_Omit) {
+// 			if ($is_Nove_Value_Numeric == true && $tds_Item_2 !== $keyWord_Omit) {
+
+			$cond_1 = $is_Nove_Value_Numeric == true;
+			$cond_2 = !($tds_Item_2->nodeValue == $keyWord_Omit);
+			
+			if ( $cond_1 && $cond_2) {
 			
 				array_push($lo_TRs_Of_Orders, $tr);
+				
+			} else {//if ($is_Nove_Value_Numeric == true)
+				
+// 				debug("(\$i = $i : \$is_Nove_Value_Numeric == false (\"" . $tr_Child_Nodes->item(0)->nodeValue . "\")");
 				
 			}//if ($is_Nove_Value_Numeric == true)
 			;
@@ -155,6 +182,27 @@ class FxUtilitiesController extends AppController {
 		debug("count(\$lo_TRs_Of_Orders) : " . count($lo_TRs_Of_Orders));
 		
 		//_20200404_140552:next
+// 		//debug
+// 		foreach ($lo_TRs_Of_Orders as $tr) {
+		
+// 			debug($tr->nodeValue);
+			
+// 			// td
+// 			$TDs = $tr->getElementsByTagName('td');
+			
+// 			debug(
+// 					"\$TDs->item(0) : " . $TDs->item(0)->nodeValue
+// 					. " / "
+// 					. "\$TDs->item(2) : " . $TDs->item(2)->nodeValue
+					
+// 					);
+// 					//'$TDs->item(0) : 4473128 / $TDs->item(2) : buy'
+			
+			
+// 		}//foreach ($lo_TRs_Of_Orders as $tr)
+		
+		
+		
 		
 // 		debug("count(\$TRs) : " . count($TRs));
 		
@@ -165,6 +213,105 @@ class FxUtilitiesController extends AppController {
 		
 // 		debug("get_class(\$html) : " . get_class($html_Object));
 // // 		debug("get_class(\$html) : " . get_class($html));
+		
+		/********************
+		 * step : 4
+		 * 		gen : csv file
+		 ********************/
+		$flg_Gen_Data_File = true;
+		
+		$dname = "data.[fx-utils]." . Utils::get_CurrentTime2(CONS::$timeLabelTypes["serial"]) . ".dir";
+		
+		if ($flg_Gen_Data_File == true) {
+			/********************
+			 * step : 4.1
+			 * 		setup
+			 ********************/
+			// paths
+			$dpath_Dst_Data_File = "C:\\WORKS_2\\WS\\Eclipse_Luna\\Cake_IFM11\\app\\webroot\\Log_Fx_Admin"
+					
+					. "\\" . $dname;
+			
+			$fname_Dst_Data_File = "Order_List." . Utils::get_CurrentTime2(CONS::$timeLabelTypes["serial"])
+								. ".dat";
+		
+			// file fullpath
+			$fpath_Dst_Data_File = join(DS, 
+						array($dpath_Dst_Data_File, $fname_Dst_Data_File));
+			
+// 			// open
+// 			$fout_Data_File = fopen($fpath_Dst_Data_File, "r");
+			
+			/********************
+			 * step : 4.2
+			 * 		build text
+			 ********************/
+			/********************
+			 * step : 4.2 : 1
+			 * 		meta
+			 ********************/
+			// text : meta
+			$txt = "\n";
+			$txt .= "\$query_Param_Dpath_Statement_File\t" . $query_Param_Dpath_Statement_File;
+			$txt .= "\n";
+			
+			$txt .= "\$query_Param_Fname_Statement_File\t$query_Param_Fname_Statement_File";
+			$txt .= "\n";
+			
+			// num of TRs
+			$txt .= "count(\$lo_TRs_Of_Orders)" . "\t" . count($lo_TRs_Of_Orders);
+			$txt .= "\n";
+			
+			// separator line
+			$txt .= "\n";
+			
+			// text : data
+			foreach ($lo_TRs_Of_Orders as $tr) {
+				/********************
+				 * step : 4.2 : 2
+				 * 		dat
+				 ********************/
+				$TDs = $tr->getElementsByTagName('td');
+				
+				$lenOf_TDs = $TDs->length;
+				
+				$lo_TD_Vals = array();
+				
+				foreach ($TDs as $td) {
+				
+					array_push($lo_TD_Vals, $td->nodeValue);
+					
+				}//foreach ($TDs as $td)
+				
+				$txt .= join("\t", $lo_TD_Vals);
+				$txt .="\n";
+				
+				
+				
+// 				$txt .= $TDs->item(0)->nodeValue;
+// 				$txt .= "\t";
+				
+// 				$txt .= $TDs->item(1)->nodeValue;
+// 				$txt .="\n";
+				
+			}//foreach ($lo_TRs_Of_Orders as $tr)
+			
+			
+			
+			
+			// write
+// 			$fout_Data_File.write($txt);
+			Utils::write_Log__Fx_Admin($dpath_Dst_Data_File, $fname_Dst_Data_File, $txt, __FILE__, __LINE__);
+			
+// 			// close file
+// 			fclose($fout_Data_File);
+			
+			debug("file written : $fpath_Dst_Data_File");
+			
+		}//if ($flg_Gen_Data_File == true)
+		;
+		
+		
 		
 		/********************
 		 * step : 3.2
