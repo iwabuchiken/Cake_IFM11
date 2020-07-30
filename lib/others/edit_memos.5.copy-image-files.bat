9e3546251294fe5cf6e59b336e643793578194a3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-07-30 11:18:45 +0900

    (Cake_IFM11) periodical addtitions

--- lib/others/edit_memos.5.copy-image-files.bat
+++ lib/others/edit_memos.5.copy-image-files.bat
@@ -7,5 +7,6 @@ REM 2019/10/28 09:12:23
 start C:\WORKS_2\WS\WS_Cake_IFM11\commands\16)_move-mov-files.bat
 REM start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\11_guitar\memos_guitar.odt
 
+exit
 
 REM pause
