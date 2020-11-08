5714a3b6784070a15209c48ef0bb156466206b55
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-11-08 09:24:44 +0900

    (Cake_IFM11) periodical addtitions

--- lib/others/edit_memos.10.insert-new-data-to-db.bat
+++ lib/others/edit_memos.10.insert-new-data-to-db.bat
@@ -11,10 +11,10 @@ REM 	open with : opera
 REM 	2019/08/13 08:12:49
 REM ================================
 :opera
-	pushd "C:\WORKS_2\Programs\opera"
+pushd "C:\WORKS_2\Programs\opera"
 
-	start launcher.exe "http://localhost/Eclipse_Luna/Cake_IFM11/images/manage_IPhone_ImageFiles?db_file=ifm11_backup_20160110_080900.bk"
-	start launcher.exe http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2
+start launcher.exe "http://localhost/Eclipse_Luna/Cake_IFM11/images/manage_IPhone_ImageFiles?db_file=ifm11_backup_20160110_080900.bk"
+start launcher.exe http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2
 
 	goto end
 
