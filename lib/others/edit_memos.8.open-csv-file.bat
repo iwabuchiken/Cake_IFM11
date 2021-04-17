671caaca6562bb1bb7e5751820c58cc4386ada38
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-04-18 08:28:31 +0900

    (Cake_IFM11) periodical addtitions

--- lib/others/edit_memos.8.open-csv-file.bat
+++ lib/others/edit_memos.8.open-csv-file.bat
@@ -1,7 +1,11 @@
 
 REM "C:\Program Files (x86)\sakura\sakura.exe" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
 REM start "C:\WORKS_2\Programs\sakura\sakura" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
-"C:\WORKS_2\Programs\sakura\sakura" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
+
+rem "C:\WORKS_2\Programs\sakura\sakura" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
+rem : 2021”N4ŒŽ18“ú8:17:37
+C:\WORKS_2\Programs\sakura\sakura C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
+
 REM "C:\WORKS_2\Programs\sakura\sakura" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
 
 REM start "C:\Program Files (x86)\sakura\sakura.exe" C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\entries.csv
