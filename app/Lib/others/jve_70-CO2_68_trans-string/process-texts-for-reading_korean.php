425ddaa2e58615da213f468979df46a10cbf9e24
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-05-18 11:52:48 +0900

    (Cake_IFM11) periodical addtitions

--- app/Lib/others/jve_70-CO2_68_trans-string/process-texts-for-reading_korean.php
+++ app/Lib/others/jve_70-CO2_68_trans-string/process-texts-for-reading_korean.php
@@ -3,11 +3,11 @@
 /*
 
 pushd C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\app\Lib\others\jve_70-CO2_68_trans-string\
-echo =============================== >> log_process-trans-sentences.log && php process-trans-sentences.php >> log_process-trans-sentences.log && echo. >> log_process-trans-sentences.log
+echo =============================== >> log_process-texts-for-reading_korean.log && php process-texts-for-reading_korean.php >> log_process-texts-for-reading_korean.log && echo. >> log_process-texts-for-reading_korean.log
 
-php process-trans-sentences.php
+php process-texts-for-reading_korean.php
 
-// echo =============================== >> log_process-trans-sentences.log && php process-trans-sentences.php >> log_process-trans-sentences.log && time >>  log_process-trans-sentences.log && echo. >> log_process-trans-sentences.log
+// echo =============================== >> log_process-texts-for-reading_korean.log && php process-texts-for-reading_korean.php >> log_process-texts-for-reading_korean.log && time >>  log_process-texts-for-reading_korean.log && echo. >> log_process-texts-for-reading_korean.log
 
 	
 	at : 20210510_172657
@@ -24,7 +24,8 @@ php process-trans-sentences.php
 
 		$tlabel = Utils::get_CurrentTime2(CONS::$timeLabelTypes["serial"]);
 		
-		echo "test_1 ($tlabel)";
+		echo "test_1 ($tlabel) : " . basename(__FILE__);
+// 		echo "test_1 ($tlabel)";
 // 		echo "test_1";
 		echo "\n";
 		echo "\n";
@@ -57,50 +58,31 @@ php process-trans-sentences.php
 		
 	}//function test_1() {
 	
-	function build_Trans_String() {
+	function build_Texts_For_Reading() {
 	
 		/********************
 		* step : 0
 		********************/
 		$tlabel = Utils::get_CurrentTime2(CONS::$timeLabelTypes["serial"]);
 	
-		echo "starting... : build_Trans_String ($tlabel)";
-		// 		echo "build_Trans_String";
-		echo "\n";
-		echo "\n";
-	
-// 		echo "CONS::\$charOf_Sort_Delimiter_CSV_Line => " . CONS::$charOf_Sort_Delimiter_CSV_Line;
-// 		echo "\n";
-// 		echo "\n";
-	
-		// 		$tlabel = Utils::get_CurrentTime2(CONS::$timeLabelTypes["serial"]);
-	
-		// 		echo $tlabel;
-		// 		echo "\n";
-		// 		echo "\n";
-	
-// 		// separator
-// 		echo "DIRECTORY_SEPARATOR => " . DIRECTORY_SEPARATOR;
-// 		echo "\n";
-// 		echo "\n";
+		#debug
+		printf("[%s : %d] starting... : build_Texts_For_Reading (%s)"
+				, basename(__FILE__), __LINE__
+				, $tlabel
+		);
+
+		print("\n");
 		
-// 		$msg	= "keyboard-memo.php ($tlabel)";
-// 		$msg	.= "\n";
-	
-// 		$dpath_Log_File = "C:\\WORKS_2\\WS\\Eclipse_Luna\\Cake_IFM11\\app\\Lib\\others\\jve_37_12.6_keyboard-memo";
-	
-// 		$fname_Log_File = "log_keyboard-memo.log";
-	
-// 		Utils::write_Log__Simple(
-	
-// 				$dpath_Log_File, $fname_Log_File
-// 				, $msg, basename(__FILE__), __LINE__);
-// 		// 				, $msg, __FILE__, __LINE__);
+// 		echo "starting... : build_Texts_For_Reading ($tlabel)";
+// 		// 		echo "build_Trans_String";
+// 		echo "\n";
+// 		echo "\n";
 
 		/********************
 		 * step : 1
 		 * 	prep : vars
 		 ********************/
+		#code:20210517_142450
 		$dpath_Trans_Dat	= "C:\\WORKS_2\\shortcuts_docs";
 		
 		$fname_Trans_Dat	= "log-session_JVE_70.[CO2].[trans-sentences].dat";
@@ -112,14 +94,18 @@ php process-trans-sentences.php
 				
 				);
 		
-// 		#debug
-// 		$message	= "\$fpath_Trans_Dat : $fpath_Trans_Dat";
+		#debug
+		$message	= "\$fpath_Trans_Dat : $fpath_Trans_Dat";
+		printf("[%s : %d]\n%s", basename(__FILE__), __LINE__, $message);
 // 		printf("[%s : %d] %s", basename(__FILE__), __LINE__, $message);
-
+		
+		print("\n");
+		
 		/********************
 		 * step : 2
 		 * 	file : open
 		 ********************/
+		#code:20210517_142744
 		$linesOf_File = file($fpath_Trans_Dat);
 
 // 		#debug
@@ -131,6 +117,228 @@ php process-trans-sentences.php
 
 // 		print_r($linesOf_File);
 
+		/********************
+		 * step : 3 : 1
+		 * 	regex : paragraph-end return char --> add a new line
+		 ********************/
+		$pattern = '/\.\r\n/';
+		
+		#prob:20210517_150538
+		$charsOf_Replacement	= ".\r\r\r\r";
+// 		$charsOf_Replacement	= ".\R\n\R\n\R\n\R\n";
+				// 		잠재적 치료효과 를 나타낸다고 보고된다.\R
+				// 		\R
+				// 		\R
+						
+// 		$charsOf_Replacement	= ".\r\n\r\n\r\n\r\n";
+// 		$charsOf_Replacement	= ".\r\n\r\n\r\n";
+// 		$charsOf_Replacement	= ".\r\n\r\n";
+// 		$charsOf_Replacement	= "\.\r\n\r\n";
+// 		$charsOf_Replacement	= '\.\r\n\r\n';
+		
+		$aryOf_Matched_Lines__Paragraph_End_Return	= array();
+		
+		#debug
+		printf("[%s : %d]\npattern --> $pattern"
+		, basename(__FILE__), __LINE__
+		);
+		
+		print("\n");
+		
+		foreach ($linesOf_File as $line) {
+			
+			#code:20210517_144312
+			
+			$result = preg_match($pattern, $line, $matches);
+// 			$result = preg_match('/\.\r\n/', $line, $matches);
+// 			$result = preg_match('/\r\n/', $line, $matches);
+			
+			//judge
+			if ($result == 1) {
+			
+				#debug
+				printf("[%s : %d]\nmatch : line --> '$line'"
+						, basename(__FILE__), __LINE__
+				);
+				
+				print_r($matches);
+				print("\n");
+				
+				#code:20210517_145120
+				# replace
+				$line_Replaced	= preg_replace($pattern, $charsOf_Replacement, $line);
+				
+				array_push($aryOf_Matched_Lines__Paragraph_End_Return, $line_Replaced);
+			
+			} else {
+			
+// 				#debug
+// 				printf("[%s : %d]\nNOT match : line --> '$line'"
+// 				, basename(__FILE__), __LINE__
+// 				);
+				
+// 				print_r($matches);
+				
+// 				print("\n");
+
+				# add line --> to a new array
+				array_push($aryOf_Matched_Lines__Paragraph_End_Return, $line);
+				
+			}//if ($result == 1)
+			
+		}//foreach ($linesOf_File as $line)
+		
+		#debug
+		printf("[%s : %d]\n\$aryOf_Matched_Lines__Paragraph_End_Return ==>"
+		, basename(__FILE__), __LINE__
+		);
+
+		print_r($aryOf_Matched_Lines__Paragraph_End_Return);
+
+		print("\n");
+
+		/********************
+		 * step : 3 : 2
+		 * 	regex : non-sentence-end return --> remove
+		 ********************/
+		$pattern = '/([^\.])\r\n/';
+		
+		$charsOf_Replacement	= "$1 ";
+		// 		$charsOf_Replacement	= '\.\r\n\r\n';
+		
+		$aryOf_Matched_Lines__Nonsentence_End_Return	= array();
+		
+		#debug
+		printf("[%s : %d]\npattern --> $pattern"
+		, basename(__FILE__), __LINE__
+		);
+		
+		print("\n");
+		
+		foreach ($aryOf_Matched_Lines__Paragraph_End_Return as $line) {
+				
+			$result = preg_match($pattern, $line, $matches);
+				
+			//judge
+			if ($result == 1) {
+					
+				#debug
+				printf("[%s : %d]\nmatch : line --> '$line'"
+				, basename(__FILE__), __LINE__
+				);
+		
+				print_r($matches);
+				print("\n");
+		
+				#code:20210517_145120
+				# replace
+				$line_Replaced	= preg_replace($pattern, $charsOf_Replacement, $line);
+		
+				array_push($aryOf_Matched_Lines__Nonsentence_End_Return, $line_Replaced);
+					
+			} else {
+					
+				// 				#debug
+				// 				printf("[%s : %d]\nNOT match : line --> '$line'"
+				// 				, basename(__FILE__), __LINE__
+				// 				);
+		
+				// 				print_r($matches);
+		
+				// 				print("\n");
+		
+				# add line --> to a new array
+				array_push($aryOf_Matched_Lines__Nonsentence_End_Return, $line);
+		
+			}//if ($result == 1)
+				
+		}//foreach ($linesOf_File as $line)
+		
+		#debug
+		printf("[%s : %d]\n\$aryOf_Matched_Lines__Nonsentence_End_Return ==>"
+				, basename(__FILE__), __LINE__
+		);
+		
+		print_r($aryOf_Matched_Lines__Nonsentence_End_Return);
+		
+		print("\n");
+
+		/********************
+		 * step : 3 : 3
+		 * 	regex : sentence-end period --> add : double return
+		 ********************/
+		#code:20210517_151904
+		
+		
+		/********************
+		 * step : 3 : 4
+		 * 	array --> join
+		 ********************/
+		$stringOf_Replaeced_Lines	= join(
+				
+				""
+				, $aryOf_Matched_Lines__Nonsentence_End_Return
+				
+				);
+		
+		#debug
+		printf("[%s : %d] \$stringOf_Replaeced_Lines ==>"
+				, basename(__FILE__), __LINE__
+		);
+		
+		print($stringOf_Replaeced_Lines);
+		print("\n");
+
+		/********************
+		 * step : 4
+		 * 	processed strings --> to a file
+		 ********************/
+		/********************
+		 * step : 4 : 1
+		 * 	vars
+		 ********************/
+		#code:20210517_151155
+		$dpath_Texts_For_Reading__Processed	= "C:\\WORKS_2\\WS\\WS_Others.JVEMV6\\JVEMV6\\70_co2\\texts-for-reading";
+		
+		$fname_Texts_For_Reading__Processed	= "out.$tlabel.txt";
+
+		$fpath_Texts_For_Reading__Processed	= join(
+		
+				DIRECTORY_SEPARATOR
+				, array($dpath_Texts_For_Reading__Processed, $fname_Texts_For_Reading__Processed)
+		
+		);
+		
+		/********************
+		 * step : 4 : 2
+		 * 	file
+		 ********************/
+		$f_Out	= fopen($fpath_Texts_For_Reading__Processed, "w");
+		
+		# write
+		$result	= fwrite($f_Out, $stringOf_Replaeced_Lines);
+		
+		# close
+		fclose($f_Out);
+		
+		#debug
+		printf("[%s : %d] file written ==>"
+				, basename(__FILE__), __LINE__
+		);
+		
+		print($fpath_Texts_For_Reading__Processed);
+		print("\n");
+
+				
+		#debug
+		printf("[%s : %d] returning..."
+				, basename(__FILE__), __LINE__
+		);
+		
+		print("\n");
+		
+		return;
+		
 		/********************
 		 * step : 3 : 0.1
 		 * 	lines : trim
@@ -177,7 +385,7 @@ php process-trans-sentences.php
 		
 // 		print_r($tokensOf_Split);
 		
-		#n:20210510_175850
+		
 		/********************
 		 * step : 4 : 1
 		 * 	tokes : filter : russian chars
@@ -259,7 +467,7 @@ php process-trans-sentences.php
 		 * step : 4 : 2
 		 * 	tokes : filter : german chars
 		 ********************/
-		#n:20210511_122722
+		
 		
 		
 // 				// report
@@ -350,29 +558,16 @@ php process-trans-sentences.php
 		exec($commnad_line);
 		
 				
-	}//function build_Trans_String() {
+	}//function build_Texts_For_Reading() {
 	
 	
-// // 	test_1();
-	
-// 	//test
-// 	var_dump($argv);
-	
-// 	var_dump("count(\$argv) => ");
-// 	var_dump(count($argv));
-	
-// // 	var_dump(get_class(count($argv)));
-	
-	
-// 	return ;
-
 	function main_operation() {
 		
 		/********************
 		* step : 1
 		********************/
 // 		test_1();
-		build_Trans_String();
+		build_Texts_For_Reading();
 		
 		
 	}//function main_operation() {
