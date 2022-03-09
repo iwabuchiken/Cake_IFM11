b417fc1161ec3f03330940147cde150d16a02989
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-03-09 11:29:19 +0900

    (Cake_IFM11) periodical addtitions

---
+++ lib/others/edit_memos.4-1.open_browser.bat
@@ -0,0 +1,14 @@
+rem : 2022”N3ŒŽ9“ú9:06:21
+rem =======================
+rem 	open : browser
+rem =======================
+pushd "C:\Program Files\Mozilla Firefox"
+
+
+set url_1="http://127.0.0.1:8000/im/"
+
+set list_of_url=%url_1%
+
+firefox.exe  %list_of_url%
+
+rem pause
