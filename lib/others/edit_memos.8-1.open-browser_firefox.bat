0420358d9727d7cfed8b2c461015d0cb07044e2b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-04-22 15:42:39 +0900

    (Cake_IFM11) periodical addtitions

---
+++ lib/others/edit_memos.8-1.open-browser_firefox.bat
@@ -0,0 +1,7 @@
+rem ================================
+rem 	2022”N4ŒŽ22“ú14:41:22
+rem 	this file used in : C:\WORKS_2\WS\WS_Cake_IFM11\commands\cake-ifm_commands.txt
+rem ================================
+"C:\Program Files\Mozilla Firefox\firefox.exe" "http://127.0.0.1:8000/im/"
+
+rem pause
---
+++ lib/others/edit_memos.8-2.restart-apps-after-memo-writing.bat
@@ -0,0 +1,8 @@
+rem ================================
+rem 	2022”N4ŒŽ22“ú14:41:22
+rem 	this file used in : C:\WORKS_2\WS\WS_Cake_IFM11\commands\cake-ifm_commands.txt
+rem ================================
+C:\WORKS_2\WS\WS_Cake_IFM11\commands\3_restart-server-8000-im.bat && C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\edit_memos.8-1.open-browser_firefox.bat && C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\edit_memos.8.open-csv-file.bat
+
+
+rem pause
