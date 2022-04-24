b06b081273568cdcdff424c66e8a59b0ac83d57c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-04-24 11:29:36 +0900

    (Cake_IFM11) periodical addtitions

--- lib/others/edit_memos.8-2.restart-apps-after-memo-writing.bat
+++ lib/others/edit_memos.8-2.restart-apps-after-memo-writing.bat
@@ -7,7 +7,8 @@ rem : to storage : 2022”N4ŒŽ23“ú11:43:50
 rem C:\WORKS_2\WS\WS_Cake_IFM11\commands\3_restart-server-8000-im.bat && C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\edit_memos.8-1.open-browser_firefox.bat && C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\edit_memos.8.open-csv-file.bat
 
 rem : browser batch --> use `start` command : 2022”N4ŒŽ23“ú11:44:15
-C:\WORKS_2\WS\WS_Cake_IFM11\commands\3_restart-server-8000-im.bat && start C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\edit_memos.8-1.open-browser_firefox.bat && C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\edit_memos.8.open-csv-file.bat
+C:\WORKS_2\WS\WS_Cake_IFM11\commands\3_restart-server-8000-im.bat && start C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\edit_memos.8-1.open-browser_firefox.bat
 
+rem C:\WORKS_2\WS\WS_Cake_IFM11\commands\3_restart-server-8000-im.bat && start C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\edit_memos.8-1.open-browser_firefox.bat && C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\lib\others\edit_memos.8.open-csv-file.bat
 
 rem pause
