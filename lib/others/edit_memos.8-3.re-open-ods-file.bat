7b52dd7b77c3bf46f8bc904d2d44f71aa9b5ae03
iwabuchiken <iwabuchi.k.2010@gmail.com>
2023-06-24 17:30:02 +0900

    (Cake_IFM11) periodical addtitions

---
+++ lib/others/edit_memos.8-3.re-open-ods-file.bat
@@ -0,0 +1,14 @@
+@echo off
+
+rem ================================
+rem 	2023”N6ŒŽ23“ú19:10:12
+rem 	this file used in : C:\WORKS_2\WS\WS_Cake_IFM11\commands\cake-ifm_commands.txt
+rem ================================
+
+echo opening ods file... : 
+
+C:\WORKS_2\batches\s.bat c1
+
+pause
+
+exit
